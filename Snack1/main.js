008b23f01d16e01b4a89872e09bda96d8fd276e3
Daniel <angelicodaniel93@gmail.com>
2022-10-25 15:28:52 +0200

    slack 1

--- Snack1/main.js
+++ Snack1/main.js
@@ -1,2 +1,10 @@
 //Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo ( del numero inserito dall'utente: esempio se il numero dell'utente è 2, il numero successivo è 3  ).
 
+let numero = parseInt(prompt('scrivi un numero'))
+
+
+if (numero % 2 == 0) {
+    console.log(numero);
+  } else if (numero % 2 != 0) {
+    console.log(numero + 1);
+  }
