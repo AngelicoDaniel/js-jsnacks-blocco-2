92b81abfff7b5ff39c48f96e3854a2437f222d0a
Daniel <angelicodaniel93@gmail.com>
2022-10-25 17:27:04 +0200

    Snack4

---
+++ Snack4/main.js
@@ -0,0 +1,6 @@
+//Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+let arr1 = ['rosso', 'verde', 'blu', 'arancione']
+let arr2 = ['azzurro', 'giallo']
+let colorAdd = arr2.push('nero', 'bianco')
+console.log(arr2)
