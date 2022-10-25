9d77ea2850aa0e7726a87ee7faeeb149a068d4bb
Daniel <angelicodaniel93@gmail.com>
2022-10-25 17:17:51 +0200

    snack3

--- Snack3/main.js
+++ Snack3/main.js
@@ -1,5 +1,10 @@
 //Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione (indice) dispari
 
-let arr = ['10', '4', '5', '8', '11']
+let numeri = [1, 2, 3, 4, 5, 6];
+let somma = 0;
+for(let i = 1; i < numeri.length; i += 2 ) {
+    somma += numeri[i];
+    console.log(somma)
+}
 
 
