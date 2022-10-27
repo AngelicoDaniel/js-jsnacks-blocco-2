a03a60e32bb79a11a50cea9e4b7afe59e06c6fff
Daniel <angelicodaniel93@gmail.com>
2022-10-27 23:38:13 +0200

    last commit

--- Snack4/main.js
+++ Snack4/main.js
@@ -1,6 +1,27 @@
 //Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
 
-let arr1 = ['rosso', 'verde', 'blu', 'arancione']
-let arr2 = ['azzurro', 'giallo']
-let colorAdd = arr2.push('nero', 'bianco')
-console.log(arr2)
+let arr1 = [1,2,3,4]
+let arr2 = [7]
+
+let bigArray
+let smallArray
+
+if (arr1.length > arr2.length){
+    bigArray = arr1
+    smallArray = arr2
+    console.log(bigArray)
+}   else if (arr1.length == arr2.length) {
+    console.log('same length')
+}   else {
+    bigArray = arr2
+    smallArray = arr1
+}
+
+let arrDifference = bigArray.length - smallArray.length
+
+for (i = 0; i < arrDifference; i++){
+    let randomNumber = Math.floor(Math.random() * 100)
+    console.log(`numero ciclo: ${i} e il numero random è ${randomNumber}`)
+    smallArray.push(randomNumber)
+}
+console.log(smallArray)
